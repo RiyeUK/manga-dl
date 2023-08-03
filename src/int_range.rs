5e6c505278805afa144f505b5466200abb3081a4
George Hollister <42khane@gmail.com>
2023-08-03 22:37:46 +0100

    Added testing and parsing to `IntRange` type.

--- src/int_range.rs
+++ src/int_range.rs
@@ -29,14 +29,6 @@ impl IntRange {
         }
     }
 
-    fn new_inclusive(start: Option<u32>, end: u32) -> Self {
-        Self {
-            start,
-            end: Some(end),
-            end_inclusive: true,
-        }
-    }
-
     fn new_range(start: u32, end: u32) -> Self {
         Self {
             start: Some(start),
@@ -59,7 +51,6 @@ impl std::str::FromStr for IntRange {
 
     fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
         let mut parts = s.split("..");
-
         let start = match parts.next() {
             Some(start_str) => {
                 if start_str.is_empty() {
@@ -71,17 +62,24 @@ impl std::str::FromStr for IntRange {
             None => bail!("Invalid range syntax"),
         };
 
+        if parts.clone().count() == 1 {}
+
         let end_and_inclusive = match parts.next() {
             Some(end_str) => {
-                if end_str.ends_with('=') {
-                    (end_str.trim_end_matches('='), true)
+                if end_str.starts_with('=') {
+                    (end_str.trim_start_matches('='), true)
                 } else {
                     (end_str, false)
                 }
             }
-            None => bail!("Invalid range syntax"),
+            None => {
+                return Ok(Self {
+                    start,
+                    end: start,
+                    end_inclusive: false,
+                })
+            }
         };
-
         let end = match end_and_inclusive.0 {
             "" => None,
             _ => Some(
@@ -108,14 +106,14 @@ mod tests {
     use std::str::FromStr;
 
     #[test]
-    fn test_parse_valid_range() -> anyhow::Result<()> {
+    fn parse_valid_range() -> anyhow::Result<()> {
         assert_eq!(IntRange::from_str("5..10")?, IntRange::new_range(5, 10));
         assert_eq!(IntRange::from_str("1..1")?, IntRange::new_range(1, 1));
         Ok(())
     }
 
     #[test]
-    fn test_parse_valid_inclusive_range() -> anyhow::Result<()> {
+    fn parse_valid_inclusive_range() -> anyhow::Result<()> {
         assert_eq!(
             IntRange::from_str("0..=100")?,
             IntRange::new_inclusive_range(0, 100)
@@ -123,36 +121,41 @@ mod tests {
 
         assert_eq!(
             IntRange::from_str("..=10")?,
-            IntRange::new(None, Some(10), false)
+            IntRange::new(None, Some(10), true)
         );
         Ok(())
     }
 
     #[test]
-    fn test_parse_valid_unbound() -> anyhow::Result<()> {
+    fn parse_valid_unbound() -> anyhow::Result<()> {
         assert_eq!(IntRange::from_str("..")?, IntRange::new(None, None, false));
         Ok(())
     }
 
     #[test]
-    fn test_parse_valid_unit() -> anyhow::Result<()> {
+    fn parse_valid_unit() -> anyhow::Result<()> {
         assert_eq!(IntRange::from_str("10")?, IntRange::new_range(10, 10));
         Ok(())
     }
 
     #[test]
-    fn test_contains_range_too_low() {
+    fn contains_range_too_low() {
         assert_eq!(IntRange::new_range(5, 10).contains(4), false);
     }
 
     #[test]
-    fn test_contains_range_start_inclusive() {
+    fn contains_range_too_high() {
+        assert_eq!(IntRange::new_range(5, 10).contains(11), false);
+    }
+
+    #[test]
+    fn contains_range_start_inclusive() {
         assert_eq!(IntRange::new_range(5, 10).contains(5), true);
         assert_eq!(IntRange::new(Some(5), None, false).contains(5), true);
     }
 
     #[test]
-    fn test_contains_range_end_inclusive() {
+    fn contains_range_end_inclusive() {
         assert_eq!(IntRange::new_inclusive_range(5, 10).contains(10), true);
         assert_eq!(IntRange::new_range(5, 10).contains(10), false);
         assert_eq!(IntRange::new(None, Some(10), true).contains(10), true);
@@ -160,9 +163,37 @@ mod tests {
     }
 
     #[test]
-    fn test_valid_range() {
+    fn valid_range() {
         assert_eq!(IntRange::new_range(5, 10).contains(6), true);
         assert_eq!(IntRange::new(None, Some(10), false).contains(1), true);
         assert_eq!(IntRange::new(None, Some(10), true).contains(6), true);
     }
+
+    #[test]
+    fn invalid_start() {
+        assert_eq!(
+            IntRange::from_str("a..10").err().unwrap().to_string(),
+            "Invalid start value"
+        );
+
+        assert_eq!(
+            IntRange::from_str("-10..10").err().unwrap().to_string(),
+            "Invalid start value"
+        );
+    }
+    // #[test]
+    // fn dont_allow_minus_numbers() {
+    //     assert_eq!(
+    //         IntRange::from_str("-10..-10").err().unwrap().to_string(),
+    //         "Minus values are not allowed"
+    //     );
+    //     assert_eq!(
+    //         IntRange::from_str("-10..10").err().unwrap().to_string(),
+    //         "Minus values are not allowed"
+    //     );
+    //     assert_eq!(
+    //         IntRange::from_str("10..-10").err().unwrap().to_string(),
+    //         "Minus values are not allowed"
+    //     );
+    // }
 }
